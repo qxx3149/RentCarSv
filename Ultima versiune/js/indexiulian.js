e8bf2e5cd8d292f5091239ff7527216c0599455e
Parasca Alexandru <48731208+qxx3149@users.noreply.github.com>
2019-06-10 20:42:40 +0300

    Versiune examen
    In aceasta versiune au fost adunate contributiile fiecarui membru din echipa noastra(incarcate in fiecare branch in parte) si puse impreuna in pentru a avea o varianta compacta a proiectului.

---
+++ Ultima versiune/js/indexiulian.js
@@ -0,0 +1,157 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+	  
+
+
+	  var email_verified = user.emailVerified;
+	  
+	  if(email_verified)
+	  {
+		  document.getElementById("verify_btn").style.display = "none";
+	  } else {
+		  document.getElementById("verify_btn").style.display = "block";
+	  }
+	  
+      document.getElementById("user_para").innerHTML = "Bine ai venit : " + email_id +
+	                                                     "<br> Email Verificat : " + email_verified;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+function create_account(){
+	var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+  
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+}
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+function send_verification()
+{
+	var user = firebase.auth().currentUser;
+
+	user.sendEmailVerification().then(function() {
+	// Email sent.
+	window.alert("Email de verificare trimis")
+	}).catch(function(error) {
+	// An error happened.
+	window.alert("Error : " +error.message);
+	});
+
+}
+function reset_password()
+{
+	var auth = firebase.auth();
+var emailAddress = document.getElementById("email_field").value;
+
+auth.sendPasswordResetEmail(emailAddress).then(function() {
+  // Email sent.
+  window.alert("Email de resetare a parolei trimis")
+}).catch(function(error) {
+  // An error happened.
+  window.alert("Error : " +error.message);
+});
+
+}
+
+var myInput = document.getElementById("password_field");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
---
+++ Ultima versiune/mail.php
@@ -0,0 +1,33 @@
+<?php
+    $to = 'emilian.lupu2@student.usv.ro';
+    $firstname = $_POST["fname"];
+    $email= $_POST["email"];
+    $text= $_POST["message"];
+    $phone= $_POST["phone"];
+    $db='mail'
+
+$db=new mysqli('localhost',$to,$firstname,$email,$text,$phone) or die("Unable to connect");
+
+echo"Great Work with your information";
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $message ='<table style="width:100%">
+        <tr>
+            <td>'.$firstname.'  '.$laststname.'</td>
+        </tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>phone: '.$phone.'</td></tr>
+        <tr><td>Text: '.$text.'</td></tr>
+
+    </table>';
+
+    if (@mail($to, $email, $message, $headers))
+    {
+        echo 'The message has been sent.';
+    }else{
+        echo 'failed';
+    }
+
+?>
